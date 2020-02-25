8255bf3ed2067a552f723e30e5b282a5a8e2b49a
pierre <pierre@pc.lan>
2020-02-26 00:21:00 +0100

    fix

--- srcs_server/lock_daemon.cpp
+++ srcs_server/lock_daemon.cpp
@@ -4,13 +4,14 @@ int		lock_daemon(int *fd)
 {
 	*fd = open(PATH_FILE_LOCK, O_WRONLY | O_CREAT);
 	if (*fd > 0)
-
+	{
 		if ((flock(*fd, LOCK_EX | LOCK_NB)) < 0)
 		{
 			close(*fd);
 			goto err;
 		}
-	return (EXIT_SUCCESS);
+		return (EXIT_SUCCESS);
+	}
 err:
 	throw Error(1, "Matt_daemon already run, file locked", 2);
 	return (EXIT_FAILURE);
